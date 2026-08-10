1df0d458bef7ff5934b4601d1c08ddfbd194654d
Pablo.Avila <pablo.avila@solera.com>
2026-08-10 10:35:21 +0200

    feat(config): register Zurbarán and La Grosella scrapers

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,17 @@
 import type { Scraper } from "./scrapers/types.js";
 import { jumarScraper } from "./scrapers/jumar.js";
 import { europa3Scraper } from "./scrapers/europa3.js";
+import { zurbaranScraper } from "./scrapers/zurbaran.js";
+import { laGrosellaScraper } from "./scrapers/lagrosella.js";
 
 /**
  * Lista de restaurantes activos.
  * Para añadir uno nuevo en el futuro: crea un fichero en src/scrapers/
  * que implemente la interfaz `Scraper` y añádelo aquí.
  */
-export const scrapers: Scraper[] = [jumarScraper, europa3Scraper];
+export const scrapers: Scraper[] = [
+  jumarScraper,
+  europa3Scraper,
+  zurbaranScraper,
+  laGrosellaScraper,
+];
